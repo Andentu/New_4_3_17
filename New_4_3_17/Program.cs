a884d7c6c7b64bb094660d4f00ce036eaaeb8fe1
Andentu <severalmaz97@gmail.com>
2023-02-14 21:42:10 +0300

    Добавлена сортировка двумерного массива  и его вывод построчно

--- New_4_3_17/Program.cs
+++ New_4_3_17/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace New_4_3_17
+﻿using System;
+
+namespace New_4_3_17
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            int num;
+            
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    {
+                        if (arr[i, j] > arr[i, k])
+                        {
+                            num = arr[i, k];
+                            arr[i, k] = arr[i, j];
+                            arr[i, j] = num;
+
+                        }
+                    }
+                }            
+            }
+            Console.WriteLine("Сортировка и вывод двумерного массива на экран построчно: ");
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int k = 0; k < arr.GetUpperBound(1) + 1; k++)
+                {
+                    Console.Write(arr[i, k] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
